f774e9b80cb22668831a9b634613e94ef782bf39
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-07-21 14:35:33 +0200

    Move unit tests to appropriate module

--- src/parser.rs
+++ src/parser.rs
@@ -219,3 +219,224 @@ fn parse_formula_expression(
     }
     result
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::parser::{parse, RExp, RFormula, RFormulaExpression, RStmt};
+
+    fn test_parse(code: &'static str) -> Vec<RStmt> {
+        parse(code).unwrap_or_else(|e| panic!("{}", e))
+    }
+
+    #[test]
+    fn parses_comments() {
+        let code = "\
+#123
+#hello
+
+# another thing   ";
+        let result = test_parse(code);
+        let expected: Vec<RStmt> = vec!["#123", "#hello", "# another thing   "]
+            .iter()
+            .map(|text| RStmt::Comment(text.to_string()))
+            .collect();
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_assignments() {
+        let code = "\
+a <- 1
+b = 2";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Assignment(RExp::variable("a"), RExp::constant("1")),
+            RStmt::Assignment(RExp::variable("b"), RExp::constant("2")),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_function_calls() {
+        let code = "\
+empty()
+single(1)
+with_args(1, x, name = value)";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Expression(RExp::Call("empty".into(), vec![])),
+
+            RStmt::Expression(RExp::Call(
+                "single".into(),
+                vec![(None, RExp::constant("1"))],
+            )),
+            RStmt::Expression(RExp::Call(
+                "with_args".into(),
+                vec![
+                    (None, RExp::constant("1")),
+                    (None, RExp::variable("x")),
+                    (Some("name".to_string()), RExp::variable("value")),
+                ],
+            )),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_strings() {
+        let code = "\
+'first'
+\"second\"";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Expression(RExp::constant("'first'")),
+            RStmt::Expression(RExp::constant("\"second\"")),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_library_calls() {
+        let code = "\
+library(plyr)
+library(MASS)";
+        let result = test_parse(code);
+        let expected = vec![RStmt::Library("plyr".into()), RStmt::Library("MASS".into())];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_indexing() {
+        let code = "\
+item$column
+item[other$thing]
+other[multiple, index, arguments]
+get_matrix()$column[1]
+item[empty,]";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Expression(RExp::Column(
+                Box::new(RExp::variable("item")),
+                Box::new(RExp::variable("column")),
+            )),
+            RStmt::Expression(RExp::Index(
+                Box::new(RExp::variable("item")),
+                vec![Some(RExp::Column(
+                    Box::new(RExp::variable("other")),
+                    Box::new(RExp::variable("thing")),
+                ))],
+            )),
+            RStmt::Expression(RExp::Index(
+                Box::new(RExp::variable("other")),
+                vec![
+                    Some(RExp::variable("multiple")),
+                    Some(RExp::variable("index")),
+                    Some(RExp::variable("arguments")),
+                ],
+            )),
+            RStmt::Expression(RExp::Index(
+                Box::new(RExp::Column(
+                    Box::new(RExp::Call("get_matrix".into(), vec![])),
+                    Box::new(RExp::variable("column")),
+                )),
+                vec![Some(RExp::constant("1"))],
+            )),
+            RStmt::Expression(RExp::Index(
+                Box::new(RExp::variable("item")),
+                vec![Some(RExp::variable("empty")), None],
+            )),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_formulae() {
+        let code = "\
+~ one_sided
+two ~ sided
+~ one + sided + multiple
+two ~ sided + multiple";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Expression(RExp::Formula(RFormula::OneSided(
+                RFormulaExpression::Variable("one_sided".into()),
+            ))),
+            RStmt::Expression(RExp::Formula(RFormula::TwoSided(
+                "two".into(),
+                RFormulaExpression::Variable("sided".into()),
+            ))),
+            RStmt::Expression(RExp::Formula(RFormula::OneSided(RFormulaExpression::Plus(
+                Box::new(RFormulaExpression::Plus(
+                    Box::new(RFormulaExpression::Variable("one".into())),
+                    "sided".into(),
+                )),
+                "multiple".into(),
+            )))),
+            RStmt::Expression(RExp::Formula(RFormula::TwoSided(
+                "two".into(),
+                RFormulaExpression::Plus(
+                    Box::new(RFormulaExpression::Variable("sided".into())),
+                    "multiple".into(),
+                ),
+            ))),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_function_definition() {
+        let code = "\
+func1 <- function () 1
+func2 <- function (with, arguments) 2
+func3 <- function (with, default = 'arguments') 3 ";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Assignment(RExp::variable("func1"), RExp::Function(vec![], "1".into())),
+            RStmt::Assignment(
+                RExp::variable("func2"),
+                RExp::Function(
+                    vec![("with".into(), None), ("arguments".into(), None)],
+                    "2".into(),
+                ),
+            ),
+            RStmt::Assignment(
+                RExp::variable("func3"),
+                RExp::Function(
+                    vec![
+                        ("with".into(), None),
+                        ("default".into(), Some(RExp::constant("'arguments'"))),
+                    ],
+                    "3".into(),
+                ),
+            ),
+        ];
+        assert_eq!(expected, result);
+    }
+
+    #[test]
+    fn parses_infix_operators() {
+        let code = "\
+1 < 3
+TRUE && FALSE
+'a' %custom% 'infix'";
+        let result = test_parse(code);
+        let expected = vec![
+            RStmt::Expression(RExp::Infix(
+                "<".into(),
+                Box::new(RExp::constant("1")),
+                Box::new(RExp::constant("3")),
+            )),
+            RStmt::Expression(RExp::Infix(
+                "&&".into(),
+                Box::new(RExp::constant("TRUE")),
+                Box::new(RExp::constant("FALSE")),
+            )),
+            RStmt::Expression(RExp::Infix(
+                "%custom%".into(),
+                Box::new(RExp::constant("'a'")),
+                Box::new(RExp::constant("'infix'")),
+            )),
+        ];
+        assert_eq!(expected, result);
+    }
+}
