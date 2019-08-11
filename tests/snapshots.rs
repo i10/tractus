ea96151c4210e05b39944b0097daba70edea31c2
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-08-11 20:59:14 +0200

    Allow hidden integration test files

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -2,6 +2,7 @@ extern crate tractus;
 
 use std::fs;
 use std::io::prelude::*;
+use std::path;
 
 use insta::assert_debug_snapshot_matches;
 
@@ -9,9 +10,11 @@ use tractus::Tractus;
 
 #[test]
 fn snapshots() {
-    let snapshot_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/snapshots");
-    let snapshot_files = fs::read_dir(snapshot_dir)
-        .unwrap()
+    let snapshot_dirs = ["tests/snapshots", "tests/snapshots/hidden"]
+        .iter()
+        .map(|relative_path| path::Path::new(env!("CARGO_MANIFEST_DIR")).join(relative_path));
+    let snapshot_files = snapshot_dirs
+        .flat_map(|snapshot_dir| fs::read_dir(snapshot_dir).unwrap())
         .filter_map(|maybe_entry| {
             maybe_entry
                 .map(|entry| {
@@ -24,13 +27,19 @@ fn snapshots() {
                 })
                 .unwrap_or(None)
         });
+
     for snapshot_path in snapshot_files {
         let mut file = fs::File::open(&snapshot_path).unwrap();
         let mut code = String::new();
         file.read_to_string(&mut code).unwrap();
 
-        let parsed =
-            Tractus::parse(&code).unwrap_or_else(|e| panic!("Parsing failed on file {}: {}", snapshot_path.to_string_lossy(), e));
+        let parsed = Tractus::parse(&code).unwrap_or_else(|e| {
+            panic!(
+                "Parsing failed on file {}: {}",
+                snapshot_path.to_string_lossy(),
+                e
+            )
+        });
         let file_stem = snapshot_path
             .as_path()
             .file_stem()
