fc89b3ee12b88c0d0b4cdca51ce6da4f0025ba4a
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-08-11 20:39:53 +0200

    Generalize tests to read files from directory

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -1,6 +1,6 @@
 extern crate tractus;
 
-use std::fs::File;
+use std::fs;
 use std::io::prelude::*;
 
 use insta::assert_debug_snapshot_matches;
@@ -8,16 +8,38 @@ use insta::assert_debug_snapshot_matches;
 use tractus::Tractus;
 
 #[test]
-fn parses_keyboard() {
-    let mut file = File::open(concat!(
-        env!("CARGO_MANIFEST_DIR"),
-        "/tests/snapshots/keyboard.R"
-    ))
-    .unwrap();
-    let mut code = String::new();
-    file.read_to_string(&mut code).unwrap();
+fn snapshots() {
+    let snapshot_dir = concat!(env!("CARGO_MANIFEST_DIR"), "/tests/snapshots");
+    let snapshot_files = fs::read_dir(snapshot_dir)
+        .unwrap()
+        .filter_map(|maybe_entry| {
+            maybe_entry
+                .map(|entry| {
+                    let path = entry.path();
+                    if path.extension()? == "R" {
+                        Some(path)
+                    } else {
+                        None
+                    }
+                })
+                .unwrap_or(None)
+        });
+    for snapshot_path in snapshot_files {
+        let mut file = fs::File::open(&snapshot_path).unwrap();
+        let mut code = String::new();
+        file.read_to_string(&mut code).unwrap();
 
-    let parsed = Tractus::parse(&code).expect("Parsing should not fail.");
-    assert_debug_snapshot_matches!(parsed);
-    assert_debug_snapshot_matches!(parsed.generate_hypothesis_tree());
+        let parsed =
+            Tractus::parse(&code).unwrap_or_else(|e| panic!("Parsing should not fail: {}", e));
+        let file_stem = snapshot_path
+            .as_path()
+            .file_stem()
+            .unwrap()
+            .to_string_lossy();
+        assert_debug_snapshot_matches!(format!("{}-parsed", file_stem), parsed);
+        assert_debug_snapshot_matches!(
+            format!("{}-dependencies", file_stem),
+            parsed.generate_hypothesis_tree()
+        );
+    }
 }
