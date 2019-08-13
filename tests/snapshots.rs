8f3b7b317782584222bed479d6b86a1093526ca6
Y0hy0h <Y0hy0h@users.noreply.github.com>
2019-08-13 14:46:03 +0200

    Hide hidden snapshot files

--- tests/snapshots.rs
+++ tests/snapshots.rs
@@ -10,13 +10,27 @@ use tractus::Tractus;
 
 #[test]
 fn snapshots() {
-    let snapshot_dirs = ["tests/snapshots", "tests/snapshots/hidden"]
-        .iter()
-        .map(get_dir);
+    test_snapshots_in(get_snapshot_dir("tests/snapshots"), None);
+}
 
-    let snapshot_files: Vec<path::PathBuf> = snapshot_dirs
-        .filter_map(|snapshot_dir| fs::read_dir(snapshot_dir).ok())
-        .flatten()
+#[test]
+fn hidden_snapshots() {
+    test_snapshots_in(get_snapshot_dir("tests/snapshots/hidden"), Some("hidden"));
+}
+
+fn get_snapshot_dir<P: std::convert::AsRef<std::path::Path>>(relative_path: P) -> path::PathBuf {
+    path::Path::new(env!("CARGO_MANIFEST_DIR")).join(relative_path)
+}
+
+fn test_snapshots_in(snapshot_dir: path::PathBuf, maybe_prefix: Option<&'static str>) {
+    let snapshot_files: Vec<path::PathBuf> = fs::read_dir(&snapshot_dir)
+        .unwrap_or_else(|e| {
+            panic!(
+                "Snapshot directory `{}` could not be read: {}",
+                snapshot_dir.display(),
+                e
+            )
+        })
         .filter_map(|maybe_entry| {
             maybe_entry
                 .map(|entry| {
@@ -32,27 +46,29 @@ fn snapshots() {
         .collect();
     assert!(!snapshot_files.is_empty(), "No snapshot files were found!");
 
-    for snapshot_path in snapshot_files {
-        let mut file = fs::File::open(&snapshot_path).unwrap();
-        let mut code = String::new();
-        file.read_to_string(&mut code).unwrap();
-
-        let parsed = Tractus::parse(&code).unwrap_or_else(|e| {
-            panic!("Parsing failed on file {}: {}", snapshot_path.display(), e)
-        });
-        let file_stem = snapshot_path
-            .as_path()
-            .file_stem()
-            .unwrap()
-            .to_string_lossy();
-        assert_debug_snapshot_matches!(format!("{}-parsed", file_stem), parsed);
-        assert_debug_snapshot_matches!(
-            format!("{}-dependencies", file_stem),
-            parsed.generate_hypothesis_tree()
-        );
+    for snapshot_file in snapshot_files {
+        test_snapshot(&snapshot_file, maybe_prefix)
     }
 }
 
-fn get_dir<P: std::convert::AsRef<std::path::Path>>(relative_path: P) -> path::PathBuf {
-    path::Path::new(env!("CARGO_MANIFEST_DIR")).join(relative_path)
+fn test_snapshot(snapshot_path: &path::PathBuf, maybe_prefix: Option<&'static str>) {
+    let mut file = fs::File::open(&snapshot_path).unwrap();
+    let mut code = String::new();
+    file.read_to_string(&mut code).unwrap();
+
+    let parsed = Tractus::parse(&code)
+        .unwrap_or_else(|e| panic!("Parsing failed on file {}: {}", snapshot_path.display(), e));
+    let file_stem = snapshot_path
+        .as_path()
+        .file_stem()
+        .unwrap()
+        .to_string_lossy();
+    let snapshot_name = maybe_prefix
+        .map(|prefix| format!("{}-{}", prefix, file_stem))
+        .unwrap_or_else(|| file_stem.into_owned());
+    assert_debug_snapshot_matches!(format!("{}-parsed", snapshot_name), parsed);
+    assert_debug_snapshot_matches!(
+        format!("{}-dependencies", snapshot_name),
+        parsed.generate_hypothesis_tree()
+    );
 }
